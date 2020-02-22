53fd6addaaaaff466ba035852a4b7161ed680354
Elvie Idaho <eidaho@at-j6.21-school.ru>
2020-02-22 20:33:11 +0300

    fractol

--- ft_get_fractol.c
+++ ft_get_fractol.c
@@ -1,18 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fractol.h                                          :+:      :+:    :+:   */
+/*   ft_get_fractol.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eidaho <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/22 20:02:19 by eidaho            #+#    #+#             */
-/*   Updated: 2020/02/22 20:02:21 by eidaho           ###   ########.fr       */
+/*   Created: 2020/02/22 20:27:49 by eidaho            #+#    #+#             */
+/*   Updated: 2020/02/22 20:27:51 by eidaho           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FRACTOL_H
-# define FRACTOL_H
+#include "includes/fractol.h"
 
-int     ft_get_fractol(void *stt, char *hh_line);
 
-#endif
+int     ft_get_fractol(void *stt, char *hh_line)
+{
+
+}
